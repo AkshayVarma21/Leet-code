11db76b87e3b66280258350db497a606da80806e
AkshayVarma21 <92862551+AkshayVarma21@users.noreply.github.com>
2022-10-13 22:00:44 +0530

    Time: 151 ms (33.88%), Space: 42.9 MB (84.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,9 +4,26 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    
-    if(s.split('').sort().join('')===t.split('').sort().join('')){
-        return true;
-};
-    return false;
+    if(s.length !== t.length)
+        return false;
+        
+    const count={}
+
+    for(let i=0;i<s.length;i++){
+        if(!count[s[i]]){
+           count[s[i]]=0;
+        }
+        if(!count[t[i]]){
+           count[t[i]]=0;
+        }
+        count[s[i]]++;
+        count[t[i]]--;
+    }
+    let status =true;
+    Object.values(count).forEach(item=>{
+        if(item !==0){
+            status =  false;
+        }
+    })
+    return status;
 };

adc5a183451c462f79d4adc5caa9f4f36d0743fa
AkshayVarma21 <92862551+AkshayVarma21@users.noreply.github.com>
2024-03-10 13:03:10 +0530

    Time: 69 ms (66.51%), Space: 52.9 MB (37.83%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    
+    let sum = 0
+    
+    for(let i=0; i< columnTitle.length; i++){
+        
+        sum = sum * 26 + (columnTitle.charCodeAt(i) - 64)
+        
+    }
+    
+ return sum;
+};
+

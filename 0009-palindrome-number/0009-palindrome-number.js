29ef31391074f2dfab482e51b02afb5b46b7c4c7
AkshayVarma21 <92862551+AkshayVarma21@users.noreply.github.com>
2024-11-19 20:10:36 +0530

    Time: 14 ms (20.01%), Space: 58.6 MB (51.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,15 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let copyNum = x;
-    let r;
-    let newNum = 0;
-    
-    while(x>0){
-        r = Math.floor(x%10)
-        x = Math.floor(x/10)
-        newNum = newNum*10 + r
-    }
-
-    return newNum === copyNum
+ return x === +x.toString().split("").reverse().join("")
 };
+

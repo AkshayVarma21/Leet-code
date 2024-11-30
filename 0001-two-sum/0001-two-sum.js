426a26d0ac589d6a96b48f56fde49604d97c58e7
AkshayVarma21 <92862551+AkshayVarma21@users.noreply.github.com>
2024-11-30 15:27:19 +0530

    Time: 1 ms (89.45%), Space: 51.8 MB (14.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,13 +5,16 @@
  */
 var twoSum = function(nums, target) {
     
-    let sum = 0;
-    for(let i =0 ;i<nums.length-1;i++)
-    for(let j=i+1; j<nums.length; j++){
-        sum = nums[i] + nums[j];
-        if(sum === target){
-            return [i,j]
+    let map = new Map();
+    for(let i =0 ;i<nums.length;i++){
+        let complement = target-nums[i]
+        
+        if(map.has(complement)){
+            return [map.get(complement),i]
         }
+        
+        map.set(nums[i],i)
+        
     }
         
         

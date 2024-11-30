8bac175e907a9046f4c461c7b68e5b5bf044f2f8
AkshayVarma21 <92862551+AkshayVarma21@users.noreply.github.com>
2024-11-30 11:51:13 +0530

    Time: 1 ms (87.63%), Space: 60.8 MB (37.13%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,25 +4,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    
     const arrLen = nums.length;
-    
-    if(k > arrLen){
-        k = k % arrLen;
+    if(k>arrLen){
+       k = k%arrLen
     }
-   rev(nums, 0, arrLen-1)
-    rev(nums, 0, k-1)
-    rev(nums, k, arrLen-1)
-    
-};
+    rev(nums, 0,arrLen-1)
+    rev(nums, 0,k-1)
+     rev(nums, k,arrLen-1)
+}
 
-const rev = (nums, left, right) =>{
+const rev = (nums, start, end)=>{
+
     let temp;
-    while(left< right){
-        temp = nums[left]
-        nums[left] = nums[right]
-        nums[right] = temp
-        left++
-        right--
-    }
+    while(start<end){
+          temp = nums[start]
+          nums[start] = nums[end]
+          nums[end] = temp
+          start++;
+          end--;
+          }
+    
 }
